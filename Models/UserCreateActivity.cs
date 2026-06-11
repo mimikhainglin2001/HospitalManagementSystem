ebf3ab1c1f731d4b90ddf6051485fb78b39fca5f
mimikhainglin2001 <mimikhainglin70@gmail.com>
2026-06-11 11:57:24 +0630

    success-message

--- Models/UserCreateActivity.cs
+++ Models/UserCreateActivity.cs
@@ -4,13 +4,13 @@ namespace HospitalManagementSystem.Models;
 public class UserCreateActivity : UserModifiedActivity
 {
     public string? CreatedById { get; set; }
-    public ApplicationUser CreatedBy { get; set; }
-    public DateTime CreatedOn { get; set;}
+    public ApplicationUser? CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set;} = DateTime.UtcNow;
 }
 
 public class UserModifiedActivity
 {
     public string? ModifiedById { get; set; }
-    public ApplicationUser ModifiedBy { get; set; }
-    public DateTime ModifiedOn { get; set;}
+    public ApplicationUser? ModifiedBy { get; set; }
+    public DateTime ModifiedOn { get; set;} = DateTime.UtcNow;
 }
